fa39005f87037ae775c6d6a622ce118a3eda7929
tatarinAV@gmail.com <aspirin1>
2022-05-31 13:33:44 +0300

    change route to resources

--- routes/api.php
+++ routes/api.php
@@ -25,25 +25,7 @@ Route::get('/', [ProductController::class, 'index']);
 
 Route::group(['middleware' => 'auth:sanctum'], function() {
     // список всех продуктов
-    Route::get('products/', [ProductController::class, 'index']);
-    // получить продукт
-    Route::get('products/{id}', [ProductController::class, 'getProduct']);
-    // добавить продукт
-    Route::post('products', [ProductController::class, 'createProduct']);
-    // обновить продукт
-    Route::put('products/{id}', [ProductController::class, 'updateProduct']);
-    // удалить продукт
-    Route::delete('products/{id}', [ProductController::class, 'deleteProduct']);
-
-
-    Route::get('discounts/', [DiscountController::class, 'index']);
-    // получить скидку
-    Route::get('discounts/{id}', [DiscountController::class, 'getDiscount']);
-    // добавить скидку
-    Route::post('discounts', [DiscountController::class, 'createDiscount']);
-    // обновить скидку
-    Route::put('discounts/{id}', [DiscountController::class, 'updateDiscount']);
-    // удалить скидку
-    Route::delete('discounts/{id}', [DiscountController::class, 'deleteDiscount']);
+    Route::resource('products', 'ProductController');
+    Route::resource('discounts', 'DiscountController');
 
 });
